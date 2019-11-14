281a816534422362b75f651ee94dabac2c91b447
Julian Centurion <julian.centurion@mercadolibre.com>
2019-11-14 17:51:55 -0300

    Render user location

--- FourSquare/FourSquare/MainViewController.swift
+++ FourSquare/FourSquare/MainViewController.swift
@@ -7,24 +7,68 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
 class MainViewController: UIViewController {
-
+    
+    let locationManager:CLLocationManager
+    
+    @IBOutlet weak var mapView: MKMapView!
+    init(locationManager:CLLocationManager){
+        self.locationManager = locationManager
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
+        locationManager.delegate = self
     }
+}
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension MainViewController: MKMapViewDelegate {
+    func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
+        if CLLocationManager.authorizationStatus() == .notDetermined {
+            let alert = UIAlertController(title: "Welcome", message: "It's look likes that is the first time you join us. We need to know where you are to offers you the best places", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { action in
+                self.locationManager.requestWhenInUseAuthorization()
+                
+            }))
+            alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: { action in
+                let alert = UIAlertController(title: ":(", message: "Unfortunaly we need your location", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default))
+                self.present(alert, animated: true, completion: nil)
+            }))
+            present(alert, animated: true, completion: nil)
+        }
     }
-    */
+}
 
+extension MainViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+            mapView.showsUserLocation = true
+            break
+        default:
+            break
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last!
+        let center = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+        self.mapView.setRegion(region, animated: true)
+        
+    }
+    
 }
