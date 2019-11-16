3f5f5232015b59c96911c4d6b117908e3fe6282f
Julian Centurion <julian.centurion@mercadolibre.com>
2019-11-16 14:29:46 -0300

    Create mock classes

---
+++ FourSquare/FourSquareTests/Mocks/FourSquareServiceMock.swift
@@ -0,0 +1,13 @@
+//
+//  FourSquareServiceMock.swift
+//  FourSquareTests
+//
+//  Created by Julian Centurion on 11/16/19.
+//  Copyright © 2019 Julian Centurion. All rights reserved.
+//
+
+import UIKit
+
+class FourSquareServiceMock: FourSquareService {
+
+}
---
+++ FourSquare/FourSquareTests/Mocks/MapPresenterMock.swift
@@ -0,0 +1,13 @@
+//
+//  MapPresenterMock.swift
+//  FourSquareTests
+//
+//  Created by Julian Centurion on 11/16/19.
+//  Copyright © 2019 Julian Centurion. All rights reserved.
+//
+
+import UIKit
+
+class MapPresenterMock: MapPresenter {
+
+}
