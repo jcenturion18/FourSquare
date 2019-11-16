eb98a4d9b8483f95d02850c5c2dcb83d8836b49d
Julian Centurion <julian.centurion@mercadolibre.com>
2019-11-16 10:55:50 -0300

    Create VenueAnnotation

---
+++ FourSquare/FourSquare/Map/VenueAnnotation.swift
@@ -0,0 +1,18 @@
+//
+//  VenueAnnotation.swift
+//  FourSquare
+//
+//  Created by Julian Centurion on 11/16/19.
+//  Copyright © 2019 Julian Centurion. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class VenueAnnotation: NSObject, MKAnnotation {
+    let coordinate: CLLocationCoordinate2D
+    
+    init(coordinate: CLLocationCoordinate2D) {
+        self.coordinate = coordinate
+    }
+}
