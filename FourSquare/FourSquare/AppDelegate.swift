206a81574a76f4de44f413609f787f3520d41f04
Julian Centurion <julian.centurion@mercadolibre.com>
2019-11-16 22:23:10 -0300

    Add navigationController

--- FourSquare/FourSquare/AppDelegate.swift
+++ FourSquare/FourSquare/AppDelegate.swift
@@ -17,9 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        let homeViewController = MapViewController()
-        window!.rootViewController = homeViewController
-        window!.makeKeyAndVisible()
+        let navigationController = UINavigationController()
+        let mainView = MapViewController() //ViewController = Name of your controller
+        navigationController.viewControllers = [mainView]
+        window!.rootViewController = navigationController
+        window?.makeKeyAndVisible()
         return true
     }
 
