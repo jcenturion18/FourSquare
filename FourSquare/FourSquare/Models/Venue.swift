74209d56ce788b70af102f4725763aac277f6547
Julian Centurion <julian.centurion@mercadolibre.com>
2019-11-16 22:57:51 -0300

    Add new data to model

--- FourSquare/FourSquare/Models/Venue.swift
+++ FourSquare/FourSquare/Models/Venue.swift
@@ -10,4 +10,16 @@ struct Venue: Codable {
     let id: String
     let name: String
     let location: Location
+    let contact: Contact?
+    let rating: Double?
+    let ratingColor: String?
+    let ratingSignals: Double?
+    let description: String?
+}
+
+struct Contact: Codable {
+    let phone: String?
+    let formattedPhone: String?
+    let twitter: String?
+    let instagram: String?
 }
