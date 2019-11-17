bbcedb97844040d5e7dfcc059eadd53f37b5f077
Julian Centurion <julian.centurion@mercadolibre.com>
2019-11-16 21:16:15 -0300

    Create new models

---
+++ FourSquare/FourSquare/Models/VenueDetailResponse.swift
@@ -0,0 +1,11 @@
+//
+//  VenueDetailResponse.swift
+//  FourSquare
+//
+//  Created by Julian Centurion on 11/16/19.
+//  Copyright © 2019 Julian Centurion. All rights reserved.
+//
+
+struct VenueDetailResponse: Codable {
+    let venue:Venue
+}
---
+++ FourSquare/FourSquare/Models/VenueDetailResult.swift
@@ -0,0 +1,12 @@
+//
+//  VenueDetailResult.swift
+//  FourSquare
+//
+//  Created by Julian Centurion on 11/16/19.
+//  Copyright © 2019 Julian Centurion. All rights reserved.
+//
+
+struct VenueDetailResult: Codable {
+    let response:VenueDetailResponse
+}
+
