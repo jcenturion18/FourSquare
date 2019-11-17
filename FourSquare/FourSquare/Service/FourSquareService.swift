b058e2ab041c9bcd7b0399bcf6697c122c7b55f2
Julian Centurion <julian.centurion@mercadolibre.com>
2019-11-16 21:16:51 -0300

    Add fetchVenueDetails

--- FourSquare/FourSquare/Service/FourSquareService.swift
+++ FourSquare/FourSquare/Service/FourSquareService.swift
@@ -18,7 +18,7 @@ class FourSquareService {
     }
     
     
-    func venues(forLocation location:CLLocation,  completion: @escaping ([Venue]?, Error?) -> Void ){
+    func fetchVenues(forLocation location:CLLocation,  completion: @escaping ([Venue]?, Error?) -> Void ){
         
         var params = Parameters()
         params["ll"] = location.toString()
@@ -35,6 +35,35 @@ class FourSquareService {
                         let JSON = try JSONDecoder().decode(SearchResult.self, from: data)
                         let venues = JSON.response.venues
                         completion(venues, nil)
+                    } catch {
+                        print("Error processing data \(error)")
+                        completion(nil, error)
+                    }
+                }
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func fetchVenueDetails(forId id:String, completion: @escaping (Venue?, Error?) -> Void ){
+        var params = Parameters()
+        params["client_id"] = "FS1TPI1BMSFSJ020DA4NH1PXD1HW50GYDQYYGQMTLPPCV1YE"
+        params["client_secret"] = "55V42KYJCFKS0AH3EFGI4RKGURAS2KBH3MZG2OV1PE5HONQV"
+        params["v"] = actualDate()
+        
+        let path = "https://api.foursquare.com/v2/venues/\(id)"
+        
+        sessionManager.request(path, method: .get, parameters: params).responseJSON { response in
+            
+            switch response.result {
+            case .success:
+                if let data = response.data{
+                    do {
+                        let JSON = try JSONDecoder().decode(VenueDetailResult.self, from: data)
+                        let venue = JSON.response.venue
+                        completion(venue, nil)
                     } catch {
                         print("Error processing data \(error)")
                         completion(nil, error)
