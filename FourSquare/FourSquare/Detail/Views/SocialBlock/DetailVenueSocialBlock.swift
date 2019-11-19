faacee68b5795cf48900c4d3ffbf9bdded2b74ce
Julian Centurion <julian.centurion@mercadolibre.com>
2019-11-19 12:55:01 -0300

    Rename

--- FourSquare/FourSquare/Detail/Views/SocialBlock/DetailVenueSocialBlock.swift
+++ FourSquare/FourSquare/Detail/Views/SocialBlock/DetailVenueSocialBlock.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class VenueDetailSocialBlock: UIView {
+class DetailVenueSocialBlock: UIView {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var stackView: UIStackView!
